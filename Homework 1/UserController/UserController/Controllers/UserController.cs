9fa362f6e18504419d267c829234df35e72132ea
Ivan Petkovski <petkovski.ivo@gmail.com>
2021-08-09 13:42:38 +0200

    First Homework - UserController

---
+++ Homework 1/UserController/UserController/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserController.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ViewResult
+        [Route("User")]
+        public IActionResult GetUser()
+        {
+            return View("User");
+        }
+
+        // RedirectResult
+        public IActionResult GetContact()
+        {
+            return RedirectToAction("Admin", "AdminController");
+        }
+
+        // EmptyResult
+        [Route("Empty")]
+        public IActionResult GetEmpty()
+        {
+            return new EmptyResult();
+        }
+
+        [Route("Id/{id}")]
+        public IActionResult GetId(int? id)
+        {
+            if (id.HasValue)
+            {
+                return View("User");
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+        }
+
+    }
+}
